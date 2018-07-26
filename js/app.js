7cb992673941238d71f6e7df553a94d861a09dfe
Marcin Bryłka <marcin_brylka@wp.pl>
2018-07-26 12:46:13 +0200

    Added menu and full rwd

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 import '../scss/main.scss';
+import '../scss/reset.css';
+import '../scss/media.scss';
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    function getDistance() {
+        let topDist = header.offsetTop;
+        return topDist;
+    }
+    const header = document.querySelector(".header");
+    const hamburger = $(".hamburger");
+    const mobileMenu = $(".mobileMenu");
+    let stuck = false;
+    window.addEventListener("scroll", function () {
+        let distance = getDistance() - window.pageYOffset;
+        let offset = window.pageYOffset;
+        if ((distance <= 0) && !stuck) {
+            header.classList.add("sticky");
+            stuck = true;
+        } else if (stuck && (offset-37 <= header.offsetTop)) {
+            header.classList.remove("sticky");
+            stuck = false;
+        }
+    });
+
+    hamburger.click(function(){
+        mobileMenu.slideToggle();
+    });
+
+});
